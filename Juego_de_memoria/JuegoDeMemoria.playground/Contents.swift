68c49c707f5ec4f6f141f7b718f876a8bfbfb910
Victor Najar <victor@MacBook-Pro-de-Victor.local>
2019-02-05 17:37:46 -0600

    Adding juego de memoria

---
+++ Juego_de_memoria/JuegoDeMemoria.playground/Contents.swift
@@ -0,0 +1,18 @@
+var rango = 0...100
+
+for i in rango{
+    if(i%5 == 0){
+        print("\(i)Bingo!!!")
+    }
+    if(i%2 == 0){
+        print("\(i)par!!!")
+    }
+    else{
+        print("\(i)impar!!!")
+    }
+    if(30 < i && i < 40){
+        print("\(i)Viva Swift!!!")
+    }
+}
+
+
